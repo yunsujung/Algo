8172cd89978e9e248d7d1e9bc27f6f33ec05f156
yunsujung <clftkdh01@gmail.com>
2025-05-13 17:09:20 +0900

    [level 0] Title: 대문자와 소문자, Time: 11.21 ms, Memory: 77.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/120893. 대문자와 소문자/대문자와 소문자.java
@@ -0,0 +1,17 @@
+class Solution {
+    public String solution(String my_string) {
+        
+        String answer = "";
+        
+        for(int i=0; i<my_string.length(); i++){
+            char ch = my_string.charAt(i);
+            
+            if(Character.isLowerCase(ch)){
+                answer += Character.toUpperCase(ch); 
+            }else{
+                answer += Character.toLowerCase(ch);
+            }
+        }
+        return answer;
+    }
+}
