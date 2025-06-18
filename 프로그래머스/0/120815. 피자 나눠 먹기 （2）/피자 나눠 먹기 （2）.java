7d5606ca52b8b5af5d0f3b29db20e072cda40e53
yunsujung <clftkdh01@gmail.com>
2025-06-18 13:54:21 +0900

    [level 0] Title: 피자 나눠 먹기 (2), Time: 0.03 ms, Memory: 85.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(int n) {
+        //6조각
+        int answer = 0;
+        for(int i=1; i<=100; i++){
+            if(n*i%6==0){
+                answer=n*i/6;
+                break;
+            }
+        }
+        return answer;
+    }
+}
